d905ac4184c97bb9958f9d92711e34969b76b85f
Pawel Piotrowicz <pawel.piotrowicz@intel.com>
2020-04-21 20:42:37 +0200

    fail reorder

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iomanip>
 #include "dnnl.hpp"
 // #include "example_utils.hpp"
 using namespace dnnl;
@@ -28,6 +29,20 @@ struct Tensor
 };
 
 
+void show(std::vector<int8_t>& v) 
+{
+      int cnt=0;
+      for(auto item : v)
+      {
+         if(cnt++ == 32)
+         {
+            std::cout << std::endl;
+            cnt=0;
+         }
+         std::cout << std::setw(3) << std::setfill(' ') << (int)item << " " ;
+      }
+      std::cout << std::endl;
+}
 
 void reorder_example(dnnl::engine::kind engine_kind=dnnl::engine::kind::cpu) {
     // Create execution dnnl::engine.
@@ -35,44 +50,45 @@ void reorder_example(dnnl::engine::kind engine_kind=dnnl::engine::kind::cpu) {
     // Create dnnl::stream.
     dnnl::stream engine_stream(engine);
     // Tensor dimensions.
-    const memory::dim N = 3, // batch size
+    const memory::dim N = 2, // batch size
             IC = 3, // channels
-            IH = 227, // tensor height
-            IW = 227; // tensor width
+            IH = 4, // tensor height
+            IW = 5; // tensor width
     // Source (src) and destination (dst) tensors dimensions.
     memory::dims src_dims = {N, IC, IH, IW};
     // Allocate buffers.
-    std::vector<float> src_data(N*IC*IH*IW);
+    std::vector<int8_t> src_data(N*IC*IH*IW);
     std::vector<int8_t> dst_data(N*IC*IH*IW);
     // Initialize src tensor.
     std::generate(src_data.begin(), src_data.end(), []() {
         static int i = 0;
-        return std::cos(i++ / 10.f);
+        return i++;
     });
+
+    show(src_data);
     // Create memory descriptors and memory objects for src and dst.
-    auto src_md = memory::desc(src_dims, dt::f32, tag::nchw);
+    auto src_md = memory::desc(src_dims, dt::s8, tag::nchw);
     auto dst_md = memory::desc(src_dims, dt::s8, tag::nhwc);
     auto src_mem = memory(src_md, engine);
     auto dst_mem = memory(dst_md, engine);
     // Write data to memory object's handle.
    // write_to_dnnl_memory(src_data.data(), src_mem);
-    // Per-channel scales.
-    std::vector<float> scales(IC);
-    std::generate(scales.begin(), scales.end(), []() {
-        static int i = 0;
-        return 64 + 5 * i++;
-    });
-    // Dimension of the dst tensor where the output scales will be applied
-    const int ic_dim = 1;
-    // Create primitive post-ops (per-channel output scales)
+    // // Per-channel scales.
+    // std::vector<float> scales(IC);
+    // std::generate(scales.begin(), scales.end(), []() {
+    //     static int i = 0;
+    //     return 64 + 5 * i++;
+    // });
+    // // Dimension of the dst tensor where the output scales will be applied
+    // const int ic_dim = 1;
+    // // Create primitive post-ops (per-channel output scales)
   
     primitive_attr reorder_attr;
-    reorder_attr.set_output_scales(0 | (1 << ic_dim), scales);
+   // reorder_attr.set_output_scales(0 | (1 << ic_dim), scales);
   
   
     // Create primitive descriptor.
-    auto reorder_pd = reorder::primitive_desc(
-            engine, src_md, engine, dst_md, reorder_attr);
+    auto reorder_pd = reorder::primitive_desc(engine, src_md, engine, dst_md);
     // Create the primitive.
     auto reorder_prim = reorder(reorder_pd);
     // Primitive arguments.
@@ -84,7 +100,8 @@ void reorder_example(dnnl::engine::kind engine_kind=dnnl::engine::kind::cpu) {
     // Wait for the computation to finalize.
     engine_stream.wait();
     // Read data from memory object's handle.
-   
+    std::cout << "---Reorder Output---" << std::endl; 
+    show(dst_data);
   
   //  read_from_dnnl_memory(dst_data.data(), dst_mem);
 }
